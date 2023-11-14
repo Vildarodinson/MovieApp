309f26b07562077f470b352dc847c51912c1e417
Vildarodinson <117757981+Vildarodinson@users.noreply.github.com>
2023-11-13 17:14:54 -0800

    feat: added movie billboard, and moviecards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className="pb-20">
+        <MovieList title="Top Movies" data={movies} />
+      </div>
     </>
   );
 }
